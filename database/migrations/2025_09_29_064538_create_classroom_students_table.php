6f14f1fc18a7308c9b2b2368b56cd255dad51d7c
ZheraSH <forcecore34@gmail.com>
2025-10-07 07:52:55 +0700

    fix: migrations, rename and delete

--- database/migrations/2025_09_29_064538_create_classroom_students_table.php
+++ database/migrations/2025_09_29_064538_create_classroom_students_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('student_classrooms', function (Blueprint $table) {
+        Schema::create('classroom_students', function (Blueprint $table) {
             $table->id();
             $table->foreignId('classroom_id')->constrained('classrooms')->onDelete('cascade');
             $table->foreignId('student_id')->constrained('students')->onDelete('cascade');
