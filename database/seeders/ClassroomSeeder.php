e17c73561e43aeb6d1f3dae3bdb35b5399b5e27a
ZheraSH <forcecore34@gmail.com>
2025-10-07 09:37:34 +0700

    test: add ClassroomSeeder unit test

--- database/seeders/ClassroomSeeder.php
+++ database/seeders/ClassroomSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\Classroom;
+use App\Models\Major;
+use App\Models\LevelClass;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Str;
 
@@ -10,25 +12,25 @@ class ClassroomSeeder extends Seeder
 {
     public function run(): void
     {
-        $classes = [
-            ['name' => 'X RPL 1', 'major_id' => 1, 'employee_id' => null, 'school_year' => 1, 'level_class_id' => 1],
-            ['name' => 'X RPL 2', 'major_id' => 1, 'employee_id' => null, 'school_year' => 1, 'level_class_id' => 1],
-            ['name' => 'XI RPL 1', 'major_id' => 1, 'employee_id' => null, 'school_year' => 1, 'level_class_id' => 2],
-            ['name' => 'XII RPL 1', 'major_id' => 1, 'employee_id' => null, 'school_year' => 1, 'level_class_id' => 3],
-        ];
+        $majors = Major::all();
+        $levels = LevelClass::all();
 
-        foreach ($classes as $c) {
-            Classroom::firstOrCreate(
-                ['slug' => Str::slug($c['name'])],
-                [
-                    'major_id' => $c['major_id'],
-                    'employee_id' => $c['employee_id'],
-                    'school_year' => $c['school_year'],
-                    'level_class_id' => $c['level_class_id'],
-                ]
-            );
+        foreach ($majors as $major) {
+            foreach ($levels as $level) {
+                $name = "{$major->name} - {$level->name}";
+                $slug = Str::slug($name);
+
+                Classroom::firstOrCreate(
+                    ['slug' => $slug],
+                    [
+                        'name' => $name,
+                        'major_id' => $major->id,
+                        'level_class_id' => $level->id,
+                        'employee_id' => null,
+                        'school_year' => 1,
+                    ]
+                );
+            }
         }
     }
 }
-
-
