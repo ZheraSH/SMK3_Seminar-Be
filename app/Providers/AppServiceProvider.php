cdb834e2494588e24845cd2934ea7b2f3a39feca
wijayawijaya01010-eng <wijayawijaya01010@gmail.com>
2025-10-21 15:30:26 +0700

    feat: adding schoolYearInterface and SchoolYearRepository code in provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,20 +2,19 @@
 
 namespace App\Providers;
 
-use App\Contracts\Interfaces\EmployeeInterface;
+use App\Contracts\Interfaces\SchoolYearInterface;
 use App\Contracts\Interfaces\ReligionInterface;
 use App\Contracts\Interfaces\StudentInterface;
 use App\Contracts\Interfaces\UserInterface;
-use App\Contracts\Repositories\EmployeeRepository;
+use App\Contracts\Repositories\SchoolYearRepository;
 use App\Contracts\Repositories\ReligionRepository;
 use App\Contracts\Repositories\StudentRepository;
 use App\Contracts\Repositories\UserRepository;
-use App\Models\Student;
-use App\Models\Employee;
+use App\Models\SchoolYear;
+use App\Models\Student; 
 use App\Models\Religion;
 use App\Models\User;
 use App\Observers\StudentObserver;
-use App\Observers\EmployeeObserver;
 use App\Observers\ReligionObserver;
 use App\Observers\UserObserver;
 use Illuminate\Support\ServiceProvider;
@@ -26,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
         UserInterface::class => UserRepository::class,
         ReligionInterface::class => ReligionRepository::class,
         StudentInterface::class => StudentRepository::class,
-        EmployeeInterface::class => EmployeeRepository::class,
+        SchoolYearInterface::class => SchoolYearRepository::class,
     ];
 
     public function register(): void
@@ -42,7 +41,6 @@ class AppServiceProvider extends ServiceProvider
         \Carbon\Carbon::setLocale('id');
 
         Student::observe(StudentObserver::class);
-        Employee::observe(EmployeeObserver::class);
         Religion::observe(ReligionObserver::class);
         User::observe(UserObserver::class);
     }
