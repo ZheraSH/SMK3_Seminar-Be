d0fb406fc4a6ded89fc83279340feabaf9d654f5
wijayawijaya01010-eng <wijayawijaya01010@gmail.com>
2025-10-24 15:02:55 +0700

    refactor: change name

--- database/migrations/2025_10_16_113613_create_school_years_table.php
+++ database/migrations/2025_10_16_113613_create_school_years_table.php
@@ -4,26 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('school_years', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->string('school_year')->unique();
+            $table->string('name')->unique();
             $table->boolean('active')->default(false);
             $table->timestamps();
             $table->softDeletes();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('school_years');
--- database/seeders/SchoolYearSeeder.php
+++ database/seeders/SchoolYearSeeder.php
@@ -2,19 +2,14 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\SchoolYear;
 
-
 class SchoolYearSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        SchoolYear::firstOrCreate(['school_year' => '2023/2024'], ['active' => false]);
-        SchoolYear::firstOrCreate(['school_year' => '2024/2025'], ['active' => true]);
+        SchoolYear::firstOrCreate(['name' => '2023/2024'], ['active' => false]);
+        SchoolYear::firstOrCreate(['name' => '2024/2025'], ['active' => true]);
     }
 }
