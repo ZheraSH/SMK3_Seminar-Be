971f25a8f766e1fddde559d1791e9894b0f49186
ZheraSH <forcecore34@gmail.com>
2025-10-26 20:43:46 +0700

    fix: migrations employee

--- database/migrations/2025_10_17_004559_create_employees_table.php
+++ database/migrations/2025_10_17_004559_create_employees_table.php
@@ -24,7 +24,6 @@ return new class extends Migration
             $table->string('birth_place');
             $table->string('address');
             $table->string('phone_number');
-            $table->boolean('active')->default(true);
             $table->softDeletes();
             $table->timestamps();
         });
