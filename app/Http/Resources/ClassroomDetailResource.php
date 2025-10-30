86d8a4b9db17c5221b895cf8b0030cfafd151fcd
ZheraSH <forcecore34@gmail.com>
2025-10-30 10:45:41 +0700

    refactor: rev code for konsistensi

--- app/Http/Resources/ClassroomDetailResource.php
+++ app/Http/Resources/ClassroomDetailResource.php
@@ -10,7 +10,7 @@ class ClassroomDetailResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            "id" => $this->id,
+            'id' => $this->id,
             'name' => $this->name,
             'slug' => $this->slug,
             'major' => $this->major?->name,
