48c2225b5cf6a925eb65f08df2ab6fb23ac43aa0
wijayawijaya01010-eng <wijayawijaya01010@gmail.com>
2025-11-10 18:48:12 +0700

    feat: add semester auto-commit scheduler

--- app/Console/Commands/Kernel.php
+++ app/Console/Commands/Kernel.php
@@ -4,17 +4,23 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Helpers\SemesterHelper;
 
 class Kernel extends ConsoleKernel
 {
-    protected $commands = [
-        \App\Console\Commands\AutoCreateSchoolYear::class,
-    ];
-
     protected function schedule(Schedule $schedule): void
     {
         $schedule->command('auto:create-schoolyear')
-            ->everyMinute()
             ->timezone('Asia/Jakarta');
+
+        $schedule->call(function () {
+            SemesterHelper::commitSemester();
+        })->dailyAt('00:00')
+          ->timezone('Asia/Jakarta');
+    }
+
+    protected function commands(): void
+    {
+        $this->load(__DIR__.'/Commands');
     }
 }
