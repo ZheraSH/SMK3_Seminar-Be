8489faa24f199380bc892cfb29b39d5a8bcc9194
ZheraSH <forcecore34@gmail.com>
2025-11-11 12:17:21 +0700

    feat: new response for paginate

--- app/Helpers/ResponseHelper.php
+++ app/Helpers/ResponseHelper.php
@@ -31,4 +31,19 @@ class ResponseHelper
             'data' => null
         ], 404);
     }
+
+    public static function pagination($data, $message = 'Success')
+    {
+        return response()->json([
+            'status' => true,
+            'message' => $message,
+            'data' => $data->items(),
+            'meta' => [
+                'current_page' => $data->currentPage(),
+                'per_page' => $data->perPage(),
+                'total' => $data->total(),
+                'last_page' => $data->lastPage(),
+            ]
+        ]);
+    }
 }
