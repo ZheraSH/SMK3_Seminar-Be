0c6e5db0267702e3934f670f54e146cbb8a6e4a0
ZheraSH <forcecore34@gmail.com>
2025-11-12 15:21:27 +0700

    feat: new traits FormatTime for resource

---
+++ app/Traits/FormatsTimeTrait.php
@@ -0,0 +1,29 @@
+<?php
+namespace App\Traits;
+
+trait FormatsTimeTrait
+{
+    private function formatTime($time): string
+    {
+        if (!$time) return '';
+        if ($time instanceof \DateTime || $time instanceof \Carbon\Carbon) {
+            return $time->format('H.i');
+        }
+        if (is_string($time) && preg_match('/^(\d{1,2}):(\d{2})/', $time, $matches)) {
+            return $matches[1] . '.' . $matches[2];
+        }
+        return (string) $time;
+    }
+
+    private function formatTimeRange($startTime, $endTime): string
+    {
+        if (!$startTime || !$endTime) {
+            return '-';
+        }
+        
+        $start = $this->formatTime($startTime);
+        $end = $this->formatTime($endTime);
+        
+        return $start && $end ? $start . ' - ' . $end : '-';
+    }
+}
