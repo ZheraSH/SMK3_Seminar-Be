2588d1585360b52f06380aead9ebc8341832fb4e
ZheraSH <forcecore34@gmail.com>
2025-11-17 16:02:09 +0700

    refactor: subject resource

--- app/Http/Resources/SubjectResource.php
+++ app/Http/Resources/SubjectResource.php
@@ -10,10 +10,8 @@ class SubjectResource extends JsonResource
       public function toArray(Request $request): array
     {
         return [
-            'id'         => $this->id,
-            'name'       => $this->name,
-            'created_at' => $this->created_at?->format('Y-m-d H:i:s'),
-            'updated_at' => $this->updated_at?->format('Y-m-d H:i:s'),
+            'id' => $this->id,
+            'name' => $this->name,
         ];
     }
 }
