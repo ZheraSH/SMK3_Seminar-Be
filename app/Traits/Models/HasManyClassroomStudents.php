3e830c45e7f6276d75dce1a93ecd7aecd3e01735
ZheraSH <forcecore34@gmail.com>
2025-11-17 09:53:59 +0700

    fix: fixing relations

--- app/Traits/Models/HasManyClassroomStudents.php
+++ app/Traits/Models/HasManyClassroomStudents.php
@@ -2,11 +2,19 @@
 namespace App\Traits\Models;
 
 use App\Models\ClassroomStudents;
+use App\Models\Student;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 trait HasManyClassroomStudents {
     public function classroomStudents(): HasMany
     {
         return $this->hasMany(ClassroomStudents::class, 'classroom_id');
+    }
+
+    public function students()
+    {
+        return $this->belongsToMany(Student::class, 'classroom_students', 'classroom_id', 'student_id')
+            ->withPivot('status')
+            ->withTimestamps();
     }
 }
