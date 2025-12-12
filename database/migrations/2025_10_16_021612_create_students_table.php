8728ee813043297a03dbc822120b7a097e73fbc2
ZheraSH <forcecore34@gmail.com>
2025-12-12 23:45:54 +0700

    fix: fixed nullable migrations

--- database/migrations/2025_10_16_021612_create_students_table.php
+++ database/migrations/2025_10_16_021612_create_students_table.php
@@ -18,8 +18,8 @@ return new class extends Migration
             $table->foreignUuid('user_id')->constrained('users')->onDelete('cascade');
             $table->string('image')->nullable();
             $table->string('nisn', 17)->unique();
-            $table->foreignUuid('religion_id')->nullable()->constrained('religions')->nullOnDelete();
-            $table->enum('gender',[GenderEnum::MALE->value, GenderEnum::FEMALE->value])->nullable();
+            $table->foreignUuid('religion_id')->constrained('religions')->nullOnDelete();
+            $table->enum('gender',[GenderEnum::MALE->value, GenderEnum::FEMALE->value]);
             $table->date('birth_date');
             $table->string('birth_place');
             $table->string('address');
