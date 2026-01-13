482c1e96db5e0e8dbe56b4575afb4a47d12d643f
ZheraSH <forcecore34@gmail.com>
2026-01-13 08:30:04 +0700

    feat: adding new resource

--- app/Http/Resources/AttendancePermissionResource.php
+++ app/Http/Resources/AttendancePermissionResource.php
@@ -14,24 +14,53 @@ class AttendancePermissionResource extends JsonResource
     {
         return [
             'id' => $this->id,
+            'student' => [
+                'id' => $this->student->id,
+                'name' => $this->student->user?->name,
+            ],
+            'classroom' => $this->getActiveClassroomData(),
             'type' => [
                 'value' => $this->type?->value,
                 'label' => $this->type?->label(),
             ],
+            'date' => [
+                'start' => $this->start_date?->format('d-M-Y'),
+                'end' => $this->end_date?->format('d-M-Y'),
+            ],
+            'counselor' => $this->formatCounselor(),
             'status' => [
                 'value' =>$this->status?->value,
                 'label' => $this->status?->label(),
             ],
             'reason' => $this->reason,
-            'counselor' => $this->formatCounselor(),
         ];
     }
 
+    private function getActiveClassroomData()
+    {
+        $activeClassroomStudent = $this->student
+            ->classroomStudents
+            ->where('status', StudentStatusEnum::ACTIVE->value)
+            ->first();
+    
+        if (!$activeClassroomStudent) {
+            return ['message' => 'Siswa belum memiliki kelas aktif'];
+        }
+    
+        $activeClassroom = $activeClassroomStudent->classroom;
+    
+        return [
+            'id' => $activeClassroom->id,
+            'name' => $activeClassroom->name,
+        ];
+    }
+    
     private function formatCounselor(): array
     {
         if (!$this->counselor_id) {
             return [
-                'message' => 'Menunggu verifikasi oleh konselor'
+                'message' => 'Menunggu verifikasi oleh konselor',
+                'status' => 'pending'
             ];
         }
 
@@ -39,6 +68,6 @@ class AttendancePermissionResource extends JsonResource
             'id' => $this->counselor->id,
             'name' => $this->counselor->user?->name,
             'verified_at' => $this->verified_at?->format('d-M-Y'),
-        ];
+        ];  
     }
 }
